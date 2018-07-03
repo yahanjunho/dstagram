02902d6e4f49aebaf656ff898e1049b0ff07f5bc
flashmaestro <masterofflash@nate.com>
2018-07-03 19:33:42 +0900

    Update models.py

--- photo/models.py
+++ photo/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+
 class Photo(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='photo_posts')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=False, default='photos/no_image.png')
@@ -18,3 +22,33 @@ class Photo(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('photo:post_detail', args=[str(self.id)])
+    
+        def save(self, *args, **kwargs):
+        is_duplicated = False
+        if self.photo:
+            try:
+                before_obj = Photo.objects.get(id=self.id)
+                if before_obj.photo == self.photo:
+                    is_duplicated = True
+            except:
+                pass
+
+        if not is_duplicated:
+            image_obj = Image.open(self.photo).convert("L")
+            new_image_io = BytesIO()
+            image_obj.save(new_image_io, format='JPEG')
+
+            temp_name = self.photo.name
+            self.photo.delete(save=False)
+            self.photo.save(temp_name, content=ContentFile(new_image_io.getvalue()), save=False)
+
+        try:
+            before_obj = Photo.objects.get(id=self.id)
+            if before_obj.photo == self.photo or is_duplicated:
+                self.photo = before_obj.photo
+            else:
+                before_obj.photo.delete(save=False)
+        except:
+            pass
+
+        super(Photo, self).save(*args, **kwargs)
